33cbee03c489aaddf35bb904fbd6393e929e1026
miswierc <michal.swierczewski@nokia.com>
2017-12-12 10:25:23 +0100

    employee

---
+++ README.md
@@ -0,0 +1,5 @@
+```sh
+go build rest_api.go
+./rest_api
+curl -X GET localhost:9999/
+```
--- rest_api.go
+++ rest_api.go
@@ -4,17 +4,37 @@ import (
     "fmt"
     "log"
     "net/http"
+    "encoding/json"
+    "github.com/gorilla/mux"
 )
+
+//str.115
+type Employee struct {
+    Title       string      `json:"title"`
+    Name        string      `json:"name"`
+    Position    []string    `json:"position"`
+}
+type Employees []Employee
 
 func homePage(w http.ResponseWriter, r *http.Request){
     fmt.Fprintf(w, "Welcome in home..")
-    fmt.Println("Endpoint Hit: homePage")
+    fmt.Println("Endpoint hit: homePage")
 }
 
 func handleRequests() {
     http.HandleFunc("/", homePage)
+    http.HandleFunc("/emp", returnAllEmployees)
     log.Fatal(http.ListenAndServe(":9999", nil))
 }
+
+func returnAllEmployees(w http.ResponseWriter, r *http.Request){
+    employees := Employees{
+        Employee{Title:"spec1", Name:"test1", Position: []string{"admin1", "dev1"}},
+        Employee{Title:"spec1", Name:"test2", Position: []string{"admin2", "ops1"}},
+    }
+    fmt.Println("Endpoint hit: returnAllEmployees")
+    json.NewEncoder(w).Encode(employees)
+}    
 
 func main() {
     handleRequests()
